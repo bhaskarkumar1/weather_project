39ae635498fba207b43e10db85c0832e0532b385
Bhaskar Kumar <thisisofficialbk@gmail.com>
2022-02-01 21:08:18 +0530

    body-parser

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Weather Project Working  
 
 
-Here we are using openweathermap API  
+## Here we are using openweathermap API  
 
 ## client browser ---request--> our server -----request--> Api server  
 ## client browser <---response-- our server <--resonse--- Api server  
@@ -10,4 +10,5 @@ Here we are using openweathermap API
 Step 1. Initialize npm init and installed express module      
 Step 2. Check the server is working or not by sennding HEllo text  
 step 3. Create https request to api  and check the resonse i.e  data it sends to the server.    
-step 4. Fetch weather data using https 
+step 4. Fetch weather data using https  
+Step 5. creating post request from website get the variable using body-parser    
--- weather.js
+++ weather.js
@@ -3,36 +3,56 @@ const app = express()
 // requiring https module
 const https= require("https")
 
+const bodyParser=require("body-parser")
+app.use(bodyParser.urlencoded({extended:true}));
+
+// add css file
+app.use(express.static(__dirname + '/public'));
+
+
 app.get("/", function(req, res){
+res.sendFile(__dirname+"/index.html");
 
-const url ="https://api.openweathermap.org/data/2.5/weather?q=patna&appid=07edf5dad8ce90aca000dbe229198dfe&units=metric"
-// create https reques to URL
-https.get(url,function(response){
-  console.log(response.statusCode);
+});
 
-
-
-  // checking the response ie data we are getting from the get request made
+app.post("/",function(req, res){
+  // console.log(req.body.cityName);
 
 
-  response.on("data",function(data){
-// this will give hexa decimal output so we now to parse with Json to string
-    // console.log(data);
-  const  WeatherData =JSON.parse(data);
-    // console.log(WeatherData);
-    const temp =WeatherData.main.temp
-    const WeatherDescription= WeatherData.weather[0].description
-    const icon= WeatherData.weather[0].icon
-    const imageUrl="https://openweathermap.org/img/wn/"+icon+"@2x.png"
 
-    // console.log(temp + WeatherDescription);
+    const query=req.body.cityName;
+    const apiKey="07edf5dad8ce90aca000dbe229198dfe"
+    const unit="metric"
 
-    res.write("<p>the weather is currently "+WeatherDescription +" </p>");
-    res.write("<h1>the temperature in london is "+temp+" degree celsius</h1>");
-    res.write("<img src="+imageUrl+">");
-    res.send()
+    const url ="https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units="+unit
+    // create https reques to URL
+    https.get(url,function(response){
+      console.log(response.statusCode);
 
-  });
+
+
+      // checking the response ie data we are getting from the get request made
+
+
+      response.on("data",function(data){
+    // this will give hexa decimal output so we now to parse with Json to string
+        // console.log(data);
+      const  WeatherData =JSON.parse(data);
+        // console.log(WeatherData);
+        const temp =WeatherData.main.temp
+        const WeatherDescription= WeatherData.weather[0].description
+        const icon= WeatherData.weather[0].icon
+        const imageUrl="https://openweathermap.org/img/wn/"+icon+"@2x.png"
+
+        // console.log(temp + WeatherDescription);
+
+        res.write("<p> the weather is currently "+WeatherDescription +"</p>");
+        res.write("<h1>the temperature in "+req.body.cityName+" is "+temp+" degree celsius</h1>");
+        res.write("<img src="+imageUrl+">");
+        res.send()
+
+
+})
 
 })
 
